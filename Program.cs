f4a96344087f38ceebc85eb01680fe94f60d4d4b
Andrew <telepnev.1997@mail.ru>
2023-01-10 15:58:21 +0700

    task2 into rec

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿Console.WriteLine($"Задайте значение N. Напишите программу, которая выведет все натуральные " +
-    "числа в промежутке от N до 1. Выполнить с помощью рекурсии");
+﻿//Console.WriteLine($"Задайте значение N. Напишите программу, которая выведет все натуральные " +
+//    "числа в промежутке от N до 1. Выполнить с помощью рекурсии");
 
-int n;
+//int n;
 
-Console.WriteLine($"Задайте значение N");
-int.TryParse(Console.ReadLine()!, out n);
+//Console.WriteLine($"Задайте значение N");
+//int.TryParse(Console.ReadLine()!, out n);
 
-void nat_numbers(int number)
-{
-    Console.Write($"{number}, ");
-    if (number == 1) return;
-    else nat_numbers(number - 1);
-}
+//void nat_numbers(int number)
+//{
+//    Console.Write($"{number}, ");
+//    if (number == 1) return;
+//    else nat_numbers(number - 1);
+//}
 
-nat_numbers(n);
-Console.WriteLine();
+//nat_numbers(n);
+//Console.WriteLine();
 
 //-------------------------------------------------------------------------------------
 
@@ -28,49 +28,42 @@ int.TryParse(Console.ReadLine()!, out left_border);
 Console.WriteLine($"Задайте значение правой границы");
 int.TryParse(Console.ReadLine()!, out right_border);
 
-void print_numbers_in_borders(int left_border, int right_border)
+int print_numbers_in_borders(int left_border, int right_border)
 {
-    int sum = 0;
-
-    while (left_border <= right_border)
+    if (left_border == right_border)
     {
-        if (left_border == right_border)
-        {
-            sum += left_border;
-            break;
-        }
-
-        sum += left_border + right_border;
-        left_border++;
-        right_border--;
+        return left_border;
     }
-    Console.WriteLine($"Сумма = {sum}");
+    else
+    {
+        return left_border + right_border + print_numbers_in_borders(++left_border, --right_border);
+    }
 }
 
-print_numbers_in_borders(left_border, right_border);
+Console.WriteLine($"Сумма = {print_numbers_in_borders(left_border, right_border)}");
 
 //-----------------------------------------------------------------------------------
 
-Console.WriteLine($"Напишите программу вычисления функции Аккермана с помощью рекурсии. " +
-    $"Даны два неотрицательных числа m и n.");
+//Console.WriteLine($"Напишите программу вычисления функции Аккермана с помощью рекурсии. " +
+//    $"Даны два неотрицательных числа m и n.");
 
-int funcAckerman(int m, int n)
-{
-    //if (m == 0)
-    //    return n + 1;
-    if (m > 0 && n == 0) 
-        return funcAckerman(m - 1, 1);
-    else if (m > 0 && n > 0)
-        return funcAckerman(m - 1, funcAckerman(m, n - 1));
-    else return n + 1;
-}
+//int funcAckerman(int m, int n)
+//{
+//    //if (m == 0)
+//    //    return n + 1;
+//    if (m > 0 && n == 0) 
+//        return funcAckerman(m - 1, 1);
+//    else if (m > 0 && n > 0)
+//        return funcAckerman(m - 1, funcAckerman(m, n - 1));
+//    else return n + 1;
+//}
 
-int first_number, second_number;
+//int first_number, second_number;
 
-Console.WriteLine($"Задайте значение m для функции Аккермана");
-int.TryParse(Console.ReadLine()!, out first_number);
-Console.WriteLine($"Задайте значение n для функции Аккермана");
-int.TryParse(Console.ReadLine()!, out second_number);
+//Console.WriteLine($"Задайте значение m для функции Аккермана");
+//int.TryParse(Console.ReadLine()!, out first_number);
+//Console.WriteLine($"Задайте значение n для функции Аккермана");
+//int.TryParse(Console.ReadLine()!, out second_number);
 
-Console.WriteLine($"Функция Аккермана от m = {first_number} и n = {second_number} " +
-    $"равна {funcAckerman(first_number, second_number)}");
+//Console.WriteLine($"Функция Аккермана от m = {first_number} и n = {second_number} " +
+//    $"равна {funcAckerman(first_number, second_number)}");
